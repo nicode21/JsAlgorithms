acfc159b5622d1f106463a75af976dbee8dda87e
nicode21 <nijatan@gmail.com>
2023-02-05 05:24:05 +0400

    add files

---
+++ main.js
@@ -0,0 +1,89 @@
+"use strict";
+
+
+
+//#region 3 - 7 e bolunenler;
+
+// let num = 63;
+
+// if (num%21==0) {
+//   console.log("bolunur");
+// }
+// else {
+// console.log("bolunmur")
+// }
+
+//#endregion
+
+//#region cutdurse cemini tap;
+
+// let num1 = 11;
+// let num2 = 14;
+
+
+// if (num1%2==0 && num2%2==0) {
+//   console.log(num1+num2);
+// }else{
+//   console.log ("ededlerin ikiside cut deyil");
+// }
+
+//#endregion
+
+//#region arasindaki tek ededlerin sayi;
+
+// let num1 = 3;
+
+// let num2 = 13;
+
+// let count = 0;
+
+// for (let i = num1; i <= num2; i++) {
+
+//   if (i % 2 == 1) {
+//     count++;
+//   }
+
+// }
+
+// console.log(count);
+
+//#endregion
+
+//#region arasindaki tek ededlerin cemi;
+
+// let num1 = 3;
+
+// let num2 = 7;
+
+// let sum = 0;
+
+// for (let i = num1; i <= num2; i++) {
+
+//   if (i % 2 == 1) {
+//     sum+=i;
+//   }
+
+// }
+
+// console.log(sum);
+
+//#endregion
+
+//#region arasindaki tek ededleri gostermek;
+
+// let num1 = 3;
+
+// let num2 = 7;
+
+// for (let i = num1; i <= num2; i++) {
+
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+
+// }
+
+//#endregion
+
+
+
